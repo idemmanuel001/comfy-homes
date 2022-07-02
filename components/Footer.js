5f893468ce53dbfaf57219310ffbda3f19c67047
idemmanuel001 <idemmanuel001@gmail.com>
2022-07-02 01:57:09 +0100

    added the footer links

--- components/Footer.js
+++ components/Footer.js
@@ -5,14 +5,17 @@ import { FaFacebookSquare, FaInstagramSquare, FaTwitterSquare, FaPinterestSquare
 export default function Footer() {
   return (
     <footer className='relative bottom-0 w-full pt-8 mt-12 bg-gray-100 border-t border-gray-500 h-max md:mt-24 '>
-      <div className="flex flex-col-reverse  px-2 md:flex-row mx-auto md:items-center md:justify-between md:max-w-screen-lg">
-        <div className="flex flex-col items-start justify-center mt-4 mb-20">
+      {/* Container */}
+      <div className="flex flex-col-reverse  px-2 md:flex-row mx-auto md:items-start md:justify-between md:max-w-screen-lg">
+        
+        {/* Quick Links */}
+        <div className="flex flex-col items-start mt-4 md:mt-0 mb-20">
           <div className="flex flex-col">
             <h3 className="text-left text-xl font-bold py-1.5  mb-4 border-b-4 border-blue-800 ">
               Quick Links
             </h3>
-            <ul className="flex flex-col mb-4 ">
-              <li className='font-semibold ' >About Us</li>
+            <ul className="flex flex-col mb-2 ">
+              <li className='font-semibold '>About Us</li>
               <li className='font-semibold '>Blog</li>
               <li className='font-semibold '>Guide</li>
               <li className='font-semibold '>FAQs</li>
@@ -35,11 +38,14 @@ export default function Footer() {
             <p className="flex font-semibold ">
               &copy; 2022 Comfy-homes Group, Inc.
             </p>
+            <p className="font-semibold  ">
+              Developed by <span className="text-blue-800 cursor-pointer"> Idoko Emmanuel</span>
+            </p>
           </div>
 
           <div className="flex items-center justify-center my-2 ">
             <FaFacebookSquare
-              className='w-6 h-6 mx-1 cursor-pointer md:w-8 md:h-8'
+              className='w-6 h-6 cursor-pointer md:w-8 md:h-8'
             />
             <FaInstagramSquare
               className='w-6 h-6 mx-1 cursor-pointer md:w-8 md:h-8'
@@ -51,15 +57,28 @@ export default function Footer() {
               className='w-6 h-6 mx-1 cursor-pointer md:w-8 md:h-8'
             />
             <FaYoutubeSquare
-              className='w-6 h-6 mx-1 cursor-pointer md:w-8 md:h-8'
+              className='w-6 h-6 cursor-pointer md:w-8 md:h-8'
             />
           </div>
 
 
         </div>
 
-        <div className="flex">
-          AdditionalInfo
+
+        <div className="flex flex-col items-start  md:h-full mt-4 md:mt-0 mb-20">
+          <h3 className="text-left max-w-fit text-xl font-bold py-1.5  mb-4 border-b-4 border-blue-800 ">
+            Properties
+          </h3>
+          <ul className="flex flex-col mb-2 ">
+            <li className='font-semibold '>Find a Property to Buy</li>
+            <li className='font-semibold '>Find a Property to Rent</li>
+            <li className='font-semibold '>Penthouses for Sale</li>
+            <li className='font-semibold '>Houses for Sale with Swimming Pool</li>
+            <li className='font-semibold '>Houses for Sale with Tennis Court</li>
+            <li className='font-semibold '>New Homes for Sale in the U.A.E</li>
+            <li className='font-semibold '>Water Side Properties for Sale in the U.A.E</li>
+
+          </ul>
         </div>
 
        
