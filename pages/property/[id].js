fdcc9ad0c87fed631a52041d02af295d6570842c
idemmanuel001 <idemmanuel001@gmail.com>
2022-07-05 03:37:43 +0100

    added the other items in the property description page

--- pages/property/[id].js
+++ pages/property/[id].js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import { FaBath, FaBed } from 'react-icons/fa';
 import { BsGridFill, BsWhatsapp, BsTelephoneFill } from 'react-icons/bs';
@@ -8,14 +9,17 @@ import ImageSlider from '../../components/ImageSlider';
 import Map from '../../components/Map';
 import BackHome from '../../components/BackHome';
 
+
 export default function PropertyDetails({ propertyDetails: { price, rentFrequency, rooms, title, baths, agency, area, isVerified, description, type, amenities, furnishingStatus, photos, geography, phoneNumber: { phone, whatsapp } } }) {
+    const [readMore, setReadMore] = useState(false)
+   
     return (
         <main className='w-full h-full mb-6 bg-white md:my-12'>
 
             <BackHome />
-            <h3 className='w-10/12 mx-auto mt-4 mb-2 md:mt-8 md:mb-6 text-lg text-gray-800 md:text-4xl font-bold text-center' > {title}</h3>
+            <h2 className='w-10/12 mx-auto mt-4 mb-2 md:mt-8 md:mb-6 text-lg text-gray-800 md:text-4xl font-bold text-center' > {title}</h2>
 
-            <section className="w-10/12 py-2  mx-auto md:max-w-screen-lg">
+            <section className="w-10/12 py-2 overflow-hidden  mx-auto md:max-w-screen-lg">
                 {/* Property photos */}
                 {photos && <ImageSlider data={photos} />}
 
@@ -27,6 +31,7 @@ export default function PropertyDetails({ propertyDetails: { price, rentFrequenc
 
                         {/* Price, Verification Status, and Basic Amenities */}
                         <div className="flex flex-col  items-start justify center mt-3 mb-2 md:mt-0 md:mb-0 font-bold">
+                            <h3 className="text-left text-lg mb-2 border-b-2 border-blue-800 ">Property Information</h3>
                             <p className='flex justify-center items-center text-center' >
                                 <span className='w-3.5 h-3.5 md:w-4 md:h-4  rounded bg-blue-600 mr-1 md:mr-2' ></span>  {rentFrequency ? 'For Rent' : 'For Sale'}
                             </p>
@@ -52,7 +57,7 @@ export default function PropertyDetails({ propertyDetails: { price, rentFrequenc
 
                         {/* Agent Details */}
                         <div className="flex flex-col my-2 md:my-0 items-start justify-center font-bold  ">
-                            <p className="text-left mb-2 ">Agency Information</p>
+                            <h3 className="text-left text-lg mb-2  border-b-2 border-blue-800">Agency Information</h3>
                             <div className="flex items-center justify-center">
                                 <div className='flex justify-center items-center rounded-full shadow shadow-gray-400'>
                                     <Image
@@ -82,23 +87,32 @@ export default function PropertyDetails({ propertyDetails: { price, rentFrequenc
                         <Map geography={geography} />
                     </div>
 
-                    <div className="flex items-center justify-center my-4 text-justify">
-                        {description}
+                    <div className="flex flex-col my-4 md:my-5">
+                        <h3 className="text-left font-bold text-lg mb-2 border-b-2 border-blue-800 w-fit">Property Description</h3>
+                        <p className=" text-justify">
+                            {readMore ? description : `${description.substring(0, 220)}...` }
+                            <button 
+                            onClick={() => setReadMore(!readMore)}
+                            className=' text-center bg-white text-blue-800 ml-2'>
+                                {readMore ? 'show less' : '  read more'}
+                            </button>
+                        </p>
                     </div>
 
-                   <div>
-                        {amenities?.map((item) => {
+                    <div className='flex justify-start flex-wrap item-center mx-auto w-full md:max-w-screen-lg'>
+                        {amenities.length && <h3 className='text-left text-lg mb-1 mr-2 font-bold border-b-2 border-blue-800 w-fit ' >Facilites:</h3>}
+                        {amenities?.map((item) => (
                             item?.amenities?.map(amenity => {
                                 return (
-                                    <div
+                                    <p
                                         key={amenity.text}
-                                        className="bg-blue-800 rounded-sm p-2 m-2">
+                                        className="bg-blue-600 text-white rounded text-center px-2 mr-2 my-1">
                                         <span>{amenity.text}</span>
-                                        {console.log(amenity.text)}
-                                    </div>
+                                       
+                                    </p>
                                 );
-                            });
-                        })}
+                            })
+                        ))}
                    </div>
                 </div>
             </section>
